a476c7edfd00b6437e2973708ac73c3572064073
Nicholas Jhirad <Nicholas.Jhirad@pinnaclesports.com>
2017-07-05 12:57:25 +0200

    CRAN Ready

--- R/midas_touch.R
+++ R/midas_touch.R
@@ -23,13 +23,15 @@ html_to_list <- function(x) {
 #' @param root an html list (from xml2 as_list).
 #'    Note: If the list is particularly deep,
 #'    you may need to set option(expressions = SOMETHING BIG)
-#' @return a function call that produces the equivalent shiny objects
+#' @return a function call that produces the equivalent shiny objects. 
+#'    This will include comments, so we recommend pre-processing to remove lines which contain html comments.
 #' @export
 #' @importFrom shiny tag
 #' @examples
 #' html <- '<div class="example"><h3>test</h3></div>'
 #' html_list <- html_to_list(html)
 #' gold <- midas_touch(html_list)
+#' library(shiny)
 #' eval(gold)
 midas_touch <- function(root) {
     xattrs <- attributes(root[[names(root)]])
@@ -41,7 +43,7 @@ midas_touch <- function(root) {
     baseroot <- root[[names(root)]]
     attributes(baseroot) <- NULL
 
-    if(length(baseroot) > 0)
+    if (length(baseroot) > 0)
         baseroot <- baseroot[sapply(baseroot, function(x) !is.list(x))]
 
     call('tag',
@@ -62,20 +64,44 @@ midas_touch <- function(root) {
 #'
 #' @param html a string of complete html
 #' @param remove_newlines whether or not to remove newlines from the string
+#' @param file default (NULL) is to return the object, if this is set it will write to file
 #'
 #' @return a function call that produces the equivalent shiny objects
 #' @export
 #'
 #' @examples
+#' 
+#' # A simple example, if xml2 can parse it
+#' # midas can touch it.
 #' html <- '<div class="example"><h3>test</h3></div>'
 #' turn_shiny(html)
-#' eval(turn_shiny(html))
-turn_shiny <- function(html, remove_newlines = TRUE) {
-    if(remove_newlines)
+#' 
+#' # Load shiny so we can evaluate shiny code
+#' library(shiny)
+#' eval(parse(text = turn_shiny(html)))
+turn_shiny <-
+    function(
+        html,
+        remove_newlines = TRUE,
+        file = NULL
+    ) {
+    if (remove_newlines)
         html <- gsub('\n', '', html)
     x <- html_to_list(html)
-    midas_touch(x)
+    out <-
+        as.character(
+            as.expression(
+                midas_touch(x)
+            )
+        )
+    cat(out, fill = TRUE)
+    if (is.null(file)) {
+        return(invisible(out))
+    } else {
+        cat(out, fill = TRUE, file = file)
+    }
 }
+
 
 
 
